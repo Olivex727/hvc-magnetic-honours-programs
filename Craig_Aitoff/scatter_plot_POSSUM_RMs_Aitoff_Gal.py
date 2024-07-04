1339fbf85d8b14e7b5ab75a8c301ca37e0e6fa25
Olivex727 <olivermwalters@gmail.com>
2024-07-04 14:43:33 +1000

    Aitoff graph correction

--- Craig_Aitoff/scatter_plot_POSSUM_RMs_Aitoff_Gal.py
+++ Craig_Aitoff/scatter_plot_POSSUM_RMs_Aitoff_Gal.py
@@ -17,11 +17,26 @@ def galactic_to_hours(x, pos):
 ### Params
 figsize = [13,  8.4]
 
+# Load collated data
+
+import sys
+sys.path.append('C://Users/olive/OneDrive - Australian National University/Honours-Olivia/Programs/honours/standard_modules')
+sys.path.append('C://Users/olive/OneDrive - Australian National University/Honours-Olivia/Programs/honours/project')
+
+from collation import collator
+
+import os
+os.chdir("./Craig_Aitoff")
+
+collated_data = collator.data_whole_sky(False, load_data=["../data_processed/proc_rms_craig_annulus","../data_processed/proc_hvcs"], h1_img="../data_catalog/hi4pi-hvc-nhi-car.fits")
+
+combined_table = collated_data["RMs"]
+
 # Data from astropy table
 rm_centres = np.array(combined_table['ra_dec_deg'])
 ra_deg = rm_centres[:, 0]
 dec_deg = rm_centres[:, 1]
-faraday_depth_radmm = combined_table['RM']
+faraday_depth_radmm = combined_table['interpolation_cor']
 
 # Convert RA/Dec to Galactic coordinates
 coords = SkyCoord(ra=ra_deg*u.degree, dec=dec_deg*u.degree, frame='icrs')
@@ -40,23 +55,24 @@ faraday_depth_radmm_sorted = faraday_depth_radmm[sorted_indices]
 
 
 # Normalize Faraday depths for color mapping
-norm = Normalize(vmin=-100, vmax=100)
+norm = Normalize(vmin=-400, vmax=400)
 
 # Plot in Aitoff projection
+colormap = plt.colormaps["seismic"]
 fig, ax = plt.subplots(figsize=figsize, subplot_kw={'projection': 'mollweide'})
-sc = ax.scatter(l, b, c=faraday_depth_radmm, marker='.', cmap='plasma', norm=norm, s=2, alpha=0.6, edgecolors='none')
+sc = ax.scatter(l, b, c=faraday_depth_radmm, marker='.', cmap=colormap, s=2, alpha=0.6, edgecolors='none', norm=norm)
 
 # Colorbar at the bottom, horizontal and thinner
-sm = plt.cm.ScalarMappable(cmap='RdBu_r', norm=norm)
+#sm = plt.cm.ScalarMappable(cmap='RdBu_r', norm=norm)
 #sm = plt.cm.ScalarMappable(cmap='plasma', norm=norm)
-sm.set_array([])
-cbar = plt.colorbar(sm, orientation='horizontal', pad=0.05, aspect=50)
-cbar.set_label('Faraday Depth (rad/m^2)', fontsize=12)
+#sm.set_array([])
+cbar = plt.colorbar(sc, orientation='horizontal', pad=0.05, aspect=50)
+cbar.set_label(r'Faraday Depth [$rad m^{-2}$]', fontsize=12)
 
 # Labels and title
 ax.set_xlabel('Galactic Longitude (l)')
 ax.set_ylabel('Galactic Latitude (b)')
-ax.set_title('POSSUM RMs @ 26 May 2024')
+#ax.set_title('POSSUM RMs @ 26 May 2024')
 
 # Make background light gray
 ax.set_facecolor('lightgray')
@@ -68,6 +84,6 @@ ax.xaxis.set_major_formatter(plt.FuncFormatter(galactic_to_hours))
 ax.grid(True, which='both', color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
 
 # Save the figure as a PDF with close borders
-plt.savefig('POSSUM_RMs_Aitoff_26May_Moll_gal_FG_corr_abs_zorder.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('../../../Resources/Figures/POSSUM_RMs_Aitoff_Craig_Annulus.jpg', format='jpg', bbox_inches='tight')
 
-plt.show()
+#plt.show()
