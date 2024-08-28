10ed6c833aa9da2004003ccf781b80501d226c17
Olivex727 <olivermwalters@gmail.com>
2024-08-28 14:23:00 +1000

    complete bootstrap?

--- KS_confirmation/craig_students_t.py
+++ KS_confirmation/craig_students_t.py
@@ -1,9 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import t as student_t
-import seaborn as sns
+#import seaborn as sns
 
-num_bootstrap_samples = 1000
+# === SETUP === #
+
+# Get RM column
+
+from astropy.io import ascii
+
+def get_RMs(file, param):
+    data = ascii.read(file)
+    col = data["RM"].data - data[param].data
+    col = col[~np.isnan(col)]
+    return col
+
+rm_column = get_RMs("../data_processed/proc_rms.ecsv", "interpolation_raw")
+rm_column2 = get_RMs("../data_processed/proc_rms.ecsv", "interpolation_cor")
+
+# Set style using seaborn
+#sns.set_style("whitegrid")
+#palette = sns.color_palette("icefire")
+
+# === MATPLOTLIB SETUP === #
+
+# Set figure size suitable for single column in a two-column paper
+plt.figure(figsize=(10, 10))
+ax = plt.gca()
+
+# Make spines less prominent
+#ax.spines['top'].set_linewidth(0.3)
+#ax.spines['right'].set_linewidth(0.3)
+#ax.spines['bottom'].set_linewidth(0.3)
+#ax.spines['left'].set_linewidth(0.3)
+
+# Add minor ticks
+#ax.minorticks_on()
+
+# Set the background color to white
+ax.set_facecolor('white')
+
+# Set grid properties and ensure it appears behind all other elements
+#ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
+#ax.set_axisbelow(True)
+
+# === BOOTSTRAP & T-DISTRIBUTION === #
+
+num_samples = 10
 
 def bootstrap_fit(data, num_bootstrap_samples):
     params_bootstrap = np.zeros((num_bootstrap_samples, 3))
@@ -18,67 +61,101 @@ def bootstrap_fit(data, num_bootstrap_samples):
     
     return params_mean, params_std
 
-# Get RM column
+# Fit the student's t-distribution to your data and plot the PDF
+def bootstrap_to_t(column, name, show=True):
+    print("Fitting t-distribution for: "+name)
+    params_mean, params_std = bootstrap_fit(column, num_samples)
+    df_mean, loc_mean, scale_mean = params_mean
+    df_std, loc_std, scale_std = params_std
 
-from astropy.io import ascii
+    if show:
+        x = np.linspace(-100, 100, 1000)
+        pdf = student_t.pdf(x, df_mean, loc_mean, scale_mean)
+        ax.plot(x, pdf, label=name, linewidth=1)
 
-data = ascii.read("../data_processed/proc_rms.ecsv")
-rm_column = data["RM"].data - data["interpolation_raw"].data #[insert RM array here]
-rm_column = rm_column[~np.isnan(rm_column)]
+    return params_mean, params_std
 
-# Set style using seaborn
-sns.set_style("whitegrid")
-palette = sns.color_palette("icefire")
+params_mean1, params_std1 = bootstrap_to_t(rm_column, "Raw Interpolation Residuals t-dist")
+params_mean2, params_std2 = bootstrap_to_t(rm_column2, "Crosshatch-Bandpassed Residuals t-dist")
 
-# Set figure size suitable for single column in a two-column paper
-plt.figure(figsize=(3.5, 2.8))
-ax = plt.gca()
-
-# Make spines less prominent
-ax.spines['top'].set_linewidth(0.3)
-ax.spines['right'].set_linewidth(0.3)
-ax.spines['bottom'].set_linewidth(0.3)
-ax.spines['left'].set_linewidth(0.3)
-
-# Add minor ticks
-ax.minorticks_on()
-
-# Set the background color to white
-ax.set_facecolor('white')
-
-# Set grid properties and ensure it appears behind all other elements
-ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
-ax.set_axisbelow(True)
+# === HISTOGRAM CREATION === #
 
 # Create a histogram
 bins = np.linspace(-100, 100, 100)
-n, bins, patches = plt.hist(rm_column, bins=bins, density=True, alpha=0.5, edgecolor='none', color='#e19f7c')
-#n2, bins2, patches2 = plt.hist(rm_column2, bins=bins, density=True, alpha=0.5, edgecolor='none', color='#7cbee1')
-
-# Fit the student's t-distribution to your data and plot the PDF
-params_mean, params_std = bootstrap_fit(rm_column, 1000)
-df_mean, loc_mean, scale_mean = params_mean
-df_std, loc_std, scale_std = params_std
-x = np.linspace(-100, 100, 1000)
-pdf = student_t.pdf(x, df_mean, loc_mean, scale_mean)
-ax.plot(x, pdf, label="Fit", linewidth=1, color=palette[4])
+n, bins, patches = plt.hist(rm_column, bins=bins, density=True, alpha=0.5, edgecolor='none', color='#e19f7c', label="Raw Interpolation Residuals")
+n2, bins2, patches2 = plt.hist(rm_column2, bins=bins, density=True, alpha=0.5, edgecolor='none', color='#7cbee1', label="Crosshatch-Bandpassed Residuals")
 
 # Adding x and y axis labels
-plt.xlabel('RRM [rad m$^{-2}$]')
+plt.xlabel(r'RM Residuals [rad m$^{-2}$]')
 plt.ylabel('Density')
 
 # Set x-axis limits
 plt.xlim(-75, 75)
 
+# === T-DISTRIBUTION OUTPUT === #
+
 # Add fit parameters in a text box above the axes
-textstr = '\n'.join((
-    r'$\mathrm{df}=%.2f \pm %.2f$' % (df_mean, df_std),
-    r'$\mathrm{loc}=%.2f \pm %.2f \, \mathrm{[rad \, m^{-2}]}$' % (loc_mean, loc_std),
-    r'$\mathrm{scale}=%.2f \pm %.2f \, \mathrm{[rad \, m^{-2}]}$' % (scale_mean, scale_std),
-))
+def make_txt(params_mean, params_std, label):
+    df_mean, loc_mean, scale_mean = params_mean
+    df_std, loc_std, scale_std = params_std
+    #textstr = '\n'.join((
+    #    r'$\mathrm{df}=%.2f \pm %.2f$' % (df_mean, df_std),
+    #    r'$\mathrm{loc}=%.2f \pm %.2f \, \mathrm{[rad \, m^{-2}]}$' % (loc_mean, loc_std),
+    #    r'$\mathrm{scale}=%.2f \pm %.2f \, \mathrm{[rad \, m^{-2}]}$' % (scale_mean, scale_std)
+    #))
+    textstr = '\n'.join((
+        str(label.upper()),
+        "|--------------------------------------|",
+        "| Parameter          | Mean   | Stdev  |",
+        "|--------------------------------------|",
+        #| Degrees of Freedom | 0.0000 | 0.0000 |
+        '| Degrees of Freedom | %.4f | %.4f |' % (df_mean, df_std),
+        '| Centerpoint        | %.4f | %.4f |' % (loc_mean, loc_std),
+        '| Internal Scale     | %.3f | %.4f |' % (scale_mean, scale_std),
+        "|--------------------------------------|"
+    ))
+    return textstr
+    
+
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-ax.text(0.48, 1.05, textstr, transform=ax.transAxes, fontsize=7,
-        verticalalignment='bottom', bbox=props)
+textstr = make_txt(params_mean1, params_std1, "Raw Interpolation Residuals t-dist") + "\n\n" + make_txt(params_mean1, params_std1, "Crosshatch-Bandpassed Residuals t-dist")
+print(textstr)
+#ax.text(0.48, 1.05, textstr, transform=ax.transAxes, fontsize=7, verticalalignment='bottom', bbox=props)
 
 # Add legend above the axes
-legend = ax.legend(["t-distribution fit", "RRM", "RM"], loc='lower left', bbox_to_anchor=(0.0, 1.05))
+#legend = ax.legend(["t-distribution fit", "RRM", "RM"], loc='lower left', bbox_to_anchor=(0.0, 1.05))
+legend = ax.legend()
+
+plt.tight_layout()
+plt.savefig("./output.png", pad_inches=0.0)
+
+# === CHI-SQUARED TEST WITH T-DISTRIBUTION === #
+
+def t_dist_values(hist, bins, params):
+    df, loc, scale = params
+    distarr = []
+    for i in range(list(len(hist))):
+        distarr.append(student_t.pdf((bins[i+1]+bins[i])/2, df, loc=loc, scale=scale))
+    return distarr
+
+from scipy.stats import chisquare as chisq
+
+thist1 = t_dist_values(n, bins, params_mean1)
+chi1 = chisq(n, thist1, ddof=2)
+
+thist2 = t_dist_values(n2, bins2, params_mean2)
+chi2 = chisq(n2, thist2, ddof=2)
+
+def make_txt_chisq(chisq, label):
+    textstr = '\n'.join((
+        str(label.upper()),
+        "----------------------",
+        #| Statistic | 0.0000 |
+        '| Statistic | %.4f |' % (chisq[0]),
+        '| p-value   | %.4f |' % (chisq[1]),
+        "----------------------"
+    ))
+    return textstr
+
+textstr = make_txt(params_mean1, params_std1, "Raw Interpolation Residuals Chi-Squared") + "\n\n" + make_txt(params_mean1, params_std1, "Crosshatch-Bandpassed Residuals Chi-Squared")
+print(textstr)
